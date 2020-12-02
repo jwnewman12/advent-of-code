e131ff64f9728855aff65408b573ed505e0d2917
John W. Newman <jwnewman12@gmail.com>
2020-12-02 01:55:36 -0500

    Add day 2, part 2

--- day02.mjs
+++ day02.mjs
@@ -10,6 +10,7 @@ const streamPasswordRecords = () => (
   })
 );
 
+// Just use a series of string spits to parse it
 const parsePasswordRecords = async function*(passwordRecords) {
   for await (const passwordRecord of passwordRecords) {
     const [policy, password] = passwordRecord.split(': ');
@@ -19,15 +20,22 @@ const parsePasswordRecords = async function*(passwordRecords) {
   }
 };
 
+// Return true if the password meets the policy, false otherwise
 const passwordConforms = ([min, max, letter, password]) => {
   const letterCount = (password.match(new RegExp(letter, "g")) || []).length;
   return min <= letterCount && letterCount <= max;
 };
 
-const countValidPasswords = async (parsedPasswords) => {
+// Return true if the password meets the policy, false otherwise
+const passwordConformsV2 = ([pos1, pos2, letter, password]) => (
+  password[pos1 - 1] === letter ^ password[pos2 - 1] === letter
+);
+
+// Return the total count of valid conforming passwords in the dataset
+const countValidPasswords = async (parsedPasswords, validator = passwordConforms) => {
   let validPasswords = 0;
   for await (const parsed of parsedPasswords) {
-    if (passwordConforms(parsed)) {
+    if (validator(parsed)) {
       ++validPasswords;
     }
   }
@@ -37,7 +45,7 @@ const countValidPasswords = async (parsedPasswords) => {
 // Just use the console
 const displayAnswer = console.log;
 
-// Output the product of the two expenses that sum to the target (2020).
+// Output the count of passwords that conform to the policy
 const part1 = async () => (
   displayAnswer(
     await countValidPasswords(
@@ -46,9 +54,19 @@ const part1 = async () => (
   )
 );
 
+// Output the count of passwords that conform to the new policy
+const part2 = async () => (
+  displayAnswer(
+    await countValidPasswords(
+      parsePasswordRecords(streamPasswordRecords()),
+      passwordConformsV2,
+    )
+  )
+);
+
 const main = async () => {
   await part1();
+  await part2();
 };
 
-// Node 14.3+ has top level await now, if you name the file .mjs
 await main();
