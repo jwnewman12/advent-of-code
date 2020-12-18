f85aad2a8a4e1ac1c62d80721ab97f91b3aadc97
John W. Newman <jwnewman12@gmail.com>
2020-12-18 02:21:12 -0500

    Start day 16 part 2

--- day07.mjs
+++ day07.mjs
@@ -29,6 +29,20 @@ const parseRule = (luggageRule) => {
 };
 
 const parseRules = async function* (luggageRules) {
+/*
+  luggageRules = [
+    'light red bags contain 1 bright white bag, 2 muted yellow bags.',
+    'dark orange bags contain 3 bright white bags, 4 muted yellow bags.',
+    'bright white bags contain 1 shiny gold bag.',
+    'muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.',
+    'shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.',
+    'dark olive bags contain 3 faded blue bags, 4 dotted black bags.',
+    'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.',
+    'faded blue bags contain no other bags.',
+    'dotted black bags contain no other bags.',
+  ];
+*/
+
   for await (const luggageRule of luggageRules) {
     yield parseRule(luggageRule);
   }
--- day16.mjs
+++ day16.mjs
@@ -5,6 +5,8 @@ const FILE_NAME = 'data/train-station-rules.txt';
 // Read the file synchronously.
 const readFile = () => fs.readFileSync(FILE_NAME, 'utf-8').split('\n')
 
+const parseTicket = (ticket) => ticket.split(',').map(Number);
+
 // Parse the file to the three parts.
 const parse = (lines) => {
   const fields = new Map();
@@ -20,9 +22,8 @@ const parse = (lines) => {
 
   const tickets = lines.slice(fieldEnd + 1);
   const ticketDivide = tickets.indexOf('');
-  const myTicket = tickets[ticketDivide - 1];
-  const nearbyTickets = tickets.slice(ticketDivide + 2)
-    .map((ticket) => ticket.split(',').map(Number));
+  const myTicket = parseTicket(tickets[ticketDivide - 1]);
+  const nearbyTickets = tickets.slice(ticketDivide + 2).map(parseTicket);
 
   return [fields, myTicket, nearbyTickets];
 };
@@ -30,16 +31,22 @@ const parse = (lines) => {
 // There's nicer approach to index the fields, and build the chain of ranges
 // that are valid, then simply check each number against that smaller dataset.
 // I am not going to bother with that unless I have to.
-const isInRange = (fields, n) => (
-  [...fields.values()].map(([min1, max1, min2, max2]) => (
-    (min1 <= n && n <= max1) || (min2 <= n && n <= max2)
-  ))
+const isInFieldRange = ([min1, max1, min2, max2], n) => (
+  (min1 <= n && n <= max1) || (min2 <= n && n <= max2)
+);
+
+const isInAnyRange = (fields, n) => (
+  [...fields.values()].map((field) => isInFieldRange(field, n))
+);
+
+const isOutOfRange = (fields, n) => (
+  !isInAnyRange(fields, n).includes(true)
 );
 
 // Return the number if it does not fall within any range ; 0 if it falls
 // within at least one range.
 const test = (fields, n) => (
-  !isInRange(fields, n).includes(true) && n || 0
+  isOutOfRange(fields, n) && n || 0
 );
 
 // Return the sum of every number of a ticket that is not valid within any range.
@@ -63,9 +70,78 @@ const part1 = ([fields,, nearbyTickets]) => (
   )
 );
 
+const generateCandidates = (fields, myTicket, nearbyTickets) => {
+  const candidates = [];
+  myTicket.forEach((_, i) => {
+    [...fields.entries()].forEach((field) => {
+      const [key, value] = field;
+      let valid = true;
+      // add myticket
+      for (let j = 0; j < nearbyTickets.length; ++j) {
+        const ticket = nearbyTickets[j];
+        if (!isInFieldRange(value, ticket[i])) {
+          valid = false;
+          console.dir([i, j, 'no']);
+          break;
+        } else {
+          console.dir([i, j, 'yes']);
+        }
+      }
+      console.dir(valid);
+
+      if (valid) {
+        candidates.push([key, i]);
+      } else {
+        console.dir('noo');
+      }
+    });
+  });
+  return candidates;
+};
+
+const filterCandidates = (candidates, fields, myTicket) => {
+  while (fields.size < candidates.length) {
+    myTicket.forEach((n, i) => {
+      const fieldsForPos = candidates.filter(([,pos]) => pos === i);
+      if (fieldsForPos.length === 1) {
+        console.dir('1');
+        const [key, pos] = fieldsForPos;
+        candidates = candidates.filter(([cf, cp]) => {
+          if (key === cf) {
+            return pos === cp;
+          }
+          return true;
+        });
+      }
+    });
+  }
+  return candidates;
+};
+
+const f = (fields, myTicket, nearbyTickets) => {
+  const candidates = generateCandidates(fields, myTicket, nearbyTickets);
+  console.dir(candidates.length);
+  const filtered = filterCandidates(candidates, fields, myTicket);
+  console.dir(candidates.length);
+  const departureFields = candidates.filter(([[key]]) => key.startsWith('departure')).map(([,pos]) => pos);
+  const t = departureFields.reduce((acc, pos) => acc * myTicket[pos], 1);
+  return t;
+};
+
+const part2 = ([fields, myTicket, nearbyTickets]) => (
+  displayAnswer(
+    f(
+      fields,
+      myTicket,
+      nearbyTickets.filter((ticket) => !isOutOfRange(fields, ticket)),
+    )
+  )
+);
+
 const main = () => {
   const data = parse(readFile());
   part1(data);
+  part2(data);
 };
 
 main();
